57ef91901153a5faafbdc0c7b00fe5bff767b7ea
Diego Montaño <31355652+DiegoMontano2705@users.noreply.github.com>
2021-06-10 01:04:10 -0500

    testing new conf

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-require('dotenv').config()
+//require('dotenv').config()
 
 const express = require('express');
-const app = new express();
+const app = express();
 const mysql = require("mysql");
 const bodyParser = require('body-parser');
-const connection = require('./db-connection');
+//const connection = require('./db-connection');
 app.use(express.json());
 
-//Controllers
+
+/*Controllers
 const homePageController = require('./server/controllers/homePage')
 const createEquipoController = require('./server/controllers/createEquipo')
 const createPrestamoController = require('./server/controllers/createPrestamo')
@@ -16,6 +17,7 @@ const createUserController = require('./server/controllers/createUser')
 const deleteEquipoController = require('./server/controllers/deleteEquipo')
 const deletePrestamoController = require('./server/controllers/deletePrestamo')
 const deleteResponsivaController = require('./server/controllers/deleteResponsiva')
+*/
 
 // Port 8080 for Google App Engine
 const port = process.env.PORT || 3050;
@@ -23,12 +25,36 @@ app.listen(port, () => {
   console.log(`TCA DB API listening on port ${port}`);
 });
 
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-04.cleardb.com',
+  user: 'b6bb37f77836e0',
+  database: 'heroku_c7c3eb92efb0828',
+  password: 'a52a42ae',
+});
+
+// database: process.env.DB_DATABASE,
+// b6bb37f77836e0:a52a42ae@us-cdbr-east-04.cleardb.com/heroku_c7c3eb92efb0828
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as thread id: ' + connection.threadId);
+});
+
+
 //Routes
 //app.get('/',homePageController);
 
 app.get('/', function (req, res) {
   //res.sendFile(path.resolve(__dirname, '../client/login.html'));
   res.send("Welcome to my api!")
+});
+
+app.get('/departamento', function (req,res){
+  const sql = 'SELECT * FROM departamento';
 });
 
 
