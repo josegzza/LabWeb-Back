d8d77b906fc3a14423cbe10a872d0dde431db288
josegzza <a01038061@itesm.mx>
2021-06-10 01:20:20 -0500

    fix bugs

--- server/db-connection.js
+++ server/db-connection.js
@@ -20,7 +20,7 @@ let connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
-  password: process.env.DB_PASS
+  password: process.env.DB_PASS,
 });
 
   connection.connect(function(err) {
--- server/index.js
+++ server/index.js
@@ -5,20 +5,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./db-connection');
 
-
-
 //configuracion global de rutas
 // app.use(require('./routes/main'));
-
-// //mysql connection
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//     host : 'localhost',
-//     user : 'me',
-//     password : 'secret',
-//     database : 'mydb'
-// });
-
 
 //Routes
 app.get('/', function (req, res) {
@@ -26,18 +14,7 @@ app.get('/', function (req, res) {
   res.send("Welcome to my api!")
 })
 
-// app.route('/db')
-//   .get(function(req, res, next) {
-//     connection.query(
-//       "SELECT * FROM `books` WHERE userId = ? LIMIT 3", req.params.userId,
-//       function(error, results, fields) {
-//         if (error) throw error;
-//         res.json(results);
-//       }
-//     );
-//   });
-
 
 // Port 8080 for Google App Engine
-app.set('port', process.env.PORT || 3000);
-app.listen(3000);
+app.set('port', process.env.PORT || 8000);
+app.listen(8000);
