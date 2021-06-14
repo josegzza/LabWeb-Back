5d8dc4e12a26d235214767b19214d972587b0f4f
josegzza <a01038061@itesm.mx>
2021-06-14 13:05:57 -0500

    endpoints inicial

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./server/db-connection');
-const {Sequelize, DataTypes} = require('sequelize')
-const sequelize = new Sequelize("mysql::memory:");
+// const {Sequelize, DataTypes} = require('sequelize')
+// const sequelize = new Sequelize("mysql::memory:");
 const cors = require('cors');
 
 //configuracion global de rutas
@@ -13,16 +13,13 @@ const cors = require('cors');
 
 //Routes
 
-
-
-
 app.get('/', function (req, res) {
   // res.sendFile(path.resolve(__dirname, '../client/login.html'));
   res.send("Welcome to my api!")
 })
 
-
-
+//---------------------------Departamento------------------------------------
+//Select all
 app.get('/departamento', function (req,res){
   const sql = 'SELECT * FROM departamento';
   connection.query(sql, (error, results) => {
@@ -35,13 +32,13 @@ app.get('/departamento', function (req,res){
   });
 });
 
-app.get('/departamento/:id', function (req,res){
-  const sql = `SELECT dpto_nombre FROM departamento WHERE dpto_id = ${id}`;
-  connection.query(sql, (error
-    , results) => {
+//departamento by id
+app.get('/departamento_id/:dept_id', function (req,res){
+  let dept_id = req.params.dept_id;
+  connection.query('SELECT * FROM departamento WHERE dpto_id = ?', dept_id, (error, result) =>{
     if (error) throw error;
-    if (results.length > 0) {
-      res.json(results);
+    if (result.length > 0) {
+      res.json(result);
     } else {
       res.send('Not result');
     }
@@ -49,20 +46,52 @@ app.get('/departamento/:id', function (req,res){
 });
 
 
-app.get('/responsiba/:id', function (req,res){
-  const sql = 'SELECT * FROM responsiva WHERE colab_id IS EQUAL';
-  connection.query(sql, (error, results) => {
+//---------------------------Responsiva------------------------------------
+//responsiva por colab_id
+app.get('/responsiva_colab/:colab_id', function (req,res){
+  let colab_id = req.params.colab_id;
+  //Safe way
+  connection.query('SELECT * FROM responsiva WHERE colab_id = ?', colab_id, (error, result) => {
     if (error) throw error;
-    if (results.length > 0) {
-      res.json(results);
+    if (result.length > 0) {
+      res.json(result);
     } else {
       res.send('Not result');
     }
   });
 });
 
-app.get('/departamento', function (req,res){
-  const sql = 'SELECT * FROM departamento';
+//responsiva por rps_id
+app.get('/responsiva/:rps_id', function (req,res){
+  let rps_id = req.params.rps_id;
+  //Safe way
+  connection.query('SELECT * FROM responsiva WHERE rps_id = ?', rps_id, (error, result) => {
+    if (error) throw error;
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+//----------------------colaborador----------------------
+//query chido
+app.get('/colaborador/:colab_id', function (req,res){
+  let colab_id = req.params.colab_id
+  //Safe way
+  connection.query('SELECT * FROM colaborador C LEFT JOIN responsiva R ON C.colab_id = R.colab_id LEFT JOIN departamento d on C.dpto_id = d.dpto_id LEFT JOIN prestamo p on R.rps_id = p.rps_id LEFT JOIN equipo e on p.eq_id = e.eq_id LEFT JOIN foto f on e.eq_id = f.eq_id LEFT JOIN sede s on C.sede_id = s.sede_id WHERE C.colab_id= ?;', colab_id, (error, result) => {
+    if (error) throw error;
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.get('/colaborador/', function (req,res){
+  const sql = 'SELECT * FROM colaborador';
   connection.query(sql, (error, results) => {
     if (error) throw error;
     if (results.length > 0) {
