bf640786d431e06a2165f6132fedd4d073d42b2d
Diego Montaño <31355652+DiegoMontano2705@users.noreply.github.com>
2021-06-14 01:40:49 -0500

    start of endpoints

--- index.js
+++ index.js
@@ -4,20 +4,75 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./server/db-connection');
+const {Sequelize, DataTypes} = require('sequelize')
+const sequelize = new Sequelize("mysql::memory:");
 const cors = require('cors');
 
 //configuracion global de rutas
 // app.use(require('./routes/main'));
 
 //Routes
-//app.get('*', function(req, res) {
-//  res.sendFile(path.join(__dirname, './client/public', 'index.html'));
-//});
+
+
+
 
 app.get('/', function (req, res) {
   // res.sendFile(path.resolve(__dirname, '../client/login.html'));
   res.send("Welcome to my api!")
 })
+
+
+
+app.get('/departamento', function (req,res){
+  const sql = 'SELECT * FROM departamento';
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.get('/departamento/:id', function (req,res){
+  const sql = `SELECT dpto_nombre FROM departamento WHERE dpto_id = ${id}`;
+  connection.query(sql, (error
+    , results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+
+app.get('/responsiba/:id', function (req,res){
+  const sql = 'SELECT * FROM responsiva WHERE colab_id IS EQUAL';
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.get('/departamento', function (req,res){
+  const sql = 'SELECT * FROM departamento';
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
 
 // ** MIDDLEWARE ** //
 const whitelist = ['http://localhost:3000', 'http://localhost:8000', 'https://tcamx-inventario.herokuapp.com/']
@@ -43,3 +98,28 @@ app.listen(8000);
 setInterval(function () {
   connection.query('SELECT 1');
 }, 5000);
+
+
+/* 
+const User = sequelize.define('colaborador', {
+  colab_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  colab_nombre : DataTypes.TEXT,
+  colab_apellido: DataTypes.TEXT,
+  colab_pseudo: DataTypes.TEXT,
+  sede_id: DataTypes.INTEGER,
+  dpto_id: DataTypes.INTEGER,
+  password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      required : true
+  }
+})
+app.put('/user', async function (req,res){
+  const diego = await User.create({colab_nombre : "Diego", colab_apellido : "Montano", colab_pseudo : "DM", sede_id : 15, dpto_id : 1, password : "hola12345"});
+  console.log("Diego auto-gen ID =", diego.colab_id);
+});
+*/
--- server/db-connection.js
+++ server/db-connection.js
@@ -1,6 +1,21 @@
 const mysql = require('mysql');
+const Sequelize = require('sequelize')
 require('dotenv').config()
 
+/*
+const connection = new Sequelize(process.env.DB_DATABASE,process.env.DB_USER,process.env.DB_PASS, {
+  host : process.env.DB_HOST,
+  dialect: 'mysql',
+})
+
+  connection.authenticate()
+  .then(() => {
+    console.log('Conectado')
+  })
+  .catch(err => {
+    console.log('No se conecto')
+  })
+*/
 let connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
